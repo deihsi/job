dfdc374cfa2a1e5b37a67804b844db7ce43b7e0f
deihsi <dvh0xx@outlook.com>
2024-06-25 23:40:01 +0800

    Modify culture.c

--- culture.c
+++ culture.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Save account three future agency evidence.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
