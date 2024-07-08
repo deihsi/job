ea1dd8b95a31cfb6ec4c4a56c69818549dcacd55
deihsi <dvh0xx@outlook.com>
2024-07-09 01:03:00 +0800

    Modify business.cpp

--- business.cpp
+++ business.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Require trip popular think night agency.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
