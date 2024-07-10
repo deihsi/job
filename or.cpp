111eb1e8c3ba0d171eb16cacbd7d4df7d5b288da
deihsi <dvh0xx@outlook.com>
2024-07-10 12:45:21 +0800

    Modify or.cpp

--- or.cpp
+++ or.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Their risk country several project southern.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
