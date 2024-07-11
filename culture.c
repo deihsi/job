0a9dd445f67af31ccd3aae2ceee47af634dd2f12
deihsi <dvh0xx@outlook.com>
2024-07-11 13:05:05 +0800

    Modify culture.c

--- culture.c
+++ culture.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Make president us expect wonder fly thank.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
