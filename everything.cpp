28e722d0fc30431a89d543c0f3c9c4c6846f81a4
deihsi <dvh0xx@outlook.com>
2024-07-30 14:36:55 +0800

    Modify everything.cpp

--- everything.cpp
+++ everything.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Beat give with.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
