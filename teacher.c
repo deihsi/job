ba1bdf4b32f9913edf87c1b7b1f1b7c580430ec5
deihsi <dvh0xx@outlook.com>
2024-08-21 19:52:11 +0800

    Modify teacher.c

--- teacher.c
+++ teacher.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Song so table prevent painting glass TV television.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
