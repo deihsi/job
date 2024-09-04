cb7e001bc3f9265b2b4876c7a7ddadcbb90f0d38
deihsi <dvh0xx@outlook.com>
2024-09-04 13:46:13 +0800

    Modify world.c

--- world.c
+++ world.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# New mean determine trade capital number floor.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
