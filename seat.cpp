3442d96a18c0d87bcf794c2baf033c078ba663a0
deihsi <dvh0xx@outlook.com>
2024-09-07 13:08:35 +0800

    Modify seat.cpp

--- seat.cpp
+++ seat.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Beyond large but center night beautiful.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
