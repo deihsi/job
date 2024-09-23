91af3e49815e6b78d6df31ad3a707ab0808609c9
deihsi <dvh0xx@outlook.com>
2024-09-23 12:04:58 +0800

    Modify adult.c

--- adult.c
+++ adult.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Section information partner morning expect after second.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
