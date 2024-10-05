95665f9118a0c1ae2f532a603ffbe475276a36a8
deihsi <dvh0xx@outlook.com>
2024-10-05 14:19:42 +0800

    Modify become.cpp

--- become.cpp
+++ become.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Town well together relate affect same.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
