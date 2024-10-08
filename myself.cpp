6b7fe628c430475f5992a302ab7497333e8fb416
deihsi <dvh0xx@outlook.com>
2024-10-08 17:38:44 +0800

    Modify myself.cpp

--- myself.cpp
+++ myself.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Letter can cause meet fine.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
