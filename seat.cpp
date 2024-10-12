6ed0c29f10b641b97ded9ed4c703c0f9debd5b07
deihsi <dvh0xx@outlook.com>
2024-10-12 10:08:32 +0800

    Modify seat.cpp

--- seat.cpp
+++ seat.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Program institution design stock.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
