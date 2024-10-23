1b2a9cee4bff4a2a146f3a0d852e833e83e35228
deihsi <dvh0xx@outlook.com>
2024-10-23 12:56:53 +0800

    Modify amount.cpp

--- amount.cpp
+++ amount.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Than reason try laugh happy avoid.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
