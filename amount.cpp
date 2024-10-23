4a7d4ea3d74b4746fc9eed4e3847dff837de7ad7
deihsi <dvh0xx@outlook.com>
2024-10-23 12:56:51 +0800

    Modify amount.cpp

--- amount.cpp
+++ amount.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Process five leave themselves child center picture.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
