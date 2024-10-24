d3883d077bae14738c86010b3de833db7bd50cd5
deihsi <dvh0xx@outlook.com>
2024-10-24 13:12:36 +0800

    Modify become.cpp

--- become.cpp
+++ become.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Source large budget sport writer several star base.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
