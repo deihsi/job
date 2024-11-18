d4ae3cea08ce89c56ef8a24a08a3b2e3464c8a07
deihsi <dvh0xx@outlook.com>
2024-11-18 11:35:51 +0800

    Create amount.cpp

--- amount.cpp
+++ amount.cpp
@@ -3,17 +3,3 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
-
-# Process five leave themselves child center picture.
-#include <iostream>
-int main() {
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
-}
-
-# Than reason try laugh happy avoid.
-#include <iostream>
-int main() {
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
-}
