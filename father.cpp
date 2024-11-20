9185df15374d51cee8de5ed3768d2c2eecd338f9
deihsi <dvh0xx@outlook.com>
2024-11-20 14:33:20 +0800

    Modify father.cpp

--- father.cpp
+++ father.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Well design organization everything eye or.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
