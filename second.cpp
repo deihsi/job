15c86a46e816618b1f04f1a6a79f63505d0a1564
deihsi <dvh0xx@outlook.com>
2024-12-15 19:17:31 +0800

    Modify second.cpp

--- second.cpp
+++ second.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Take close bag defense remember successful discussion.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
