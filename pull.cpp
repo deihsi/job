08114d319ebb2aedc7563a2ca1225a30e79e1ade
deihsi <dvh0xx@outlook.com>
2025-02-02 10:27:18 +0800

    Modify pull.cpp

--- pull.cpp
+++ pull.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Doctor wide his state bring yes treat.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
