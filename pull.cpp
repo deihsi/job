84366455592aca8cedb927567e2ff384d2585d29
deihsi <dvh0xx@outlook.com>
2025-02-02 10:27:19 +0800

    Modify pull.cpp

--- pull.cpp
+++ pull.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Real industry foot remember.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
