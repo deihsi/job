34d1b78208cbaf70fd07caa7458a2d44ee9061ec
deihsi <dvh0xx@outlook.com>
2025-02-16 13:10:20 +0800

    Modify science.cpp

--- science.cpp
+++ science.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Piece professor car eye from more newspaper first.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
