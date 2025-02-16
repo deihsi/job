568ebb048f2f5225d0f48a922e7b0d7ff822e5bb
deihsi <dvh0xx@outlook.com>
2025-02-16 13:10:20 +0800

    Modify seat.cpp

--- seat.cpp
+++ seat.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Member tonight song bag include cup however.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
